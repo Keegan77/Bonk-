e7ebda75ad417e85642fedd2dccbe7431ff91809
Keegan77 <keegansb77@gmail.com>
2021-10-27 22:35:44 -0400

    Started movement and then my brain started rotting... im doing this tmr

---
+++ teamgame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+
+    public LayerMask whatIsGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+    private void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+}
