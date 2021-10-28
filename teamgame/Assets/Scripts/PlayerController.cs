84936a00345703c2d0740ff188a7dbcb1cdbb416
Keegan <57238792+Keegan77@users.noreply.github.com>
2021-10-28 10:51:39 -0400

    Added Jumping

--- teamgame/Assets/Scripts/PlayerController.cs
+++ teamgame/Assets/Scripts/PlayerController.cs
@@ -4,35 +4,88 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Components")]
     private Rigidbody2D rb;
     public float speed;
     public float jumpForce;
     private float moveInput;
 
+    [Header("Layar Mask")]
     private bool isGrounded;
     public Transform feetPos;
     public float checkRadius;
-
     public LayerMask whatIsGround;
-    // Start is called before the first frame update
+
+    [Header("Jump")]
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+
+    [Header("fall physics")]
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
+    //Gets Rigidbody component
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+    //Moves player on x axis
 
-        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = Vector2.up * jumpForce;
-        }
-    }
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+        if (moveInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        //cool jump fall
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+
+        else if (rb.velocity.y > 0 && Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+        //fixed double jump bug
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+
+        //lets player jump
+
+        if (isGrounded == true && Input.GetKeyDown("space") && isJumping == false)
+        { 
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        //makes you jump higher when you hold down space
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            { 
+                isJumping = false;
+            }
+        }
     }
 }
